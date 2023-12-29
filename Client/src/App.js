1663b9c09713c89f055dc7797ea2677d4c4a358e
Lautaro Bunkowski <lautibunko@gmail.com>
2023-12-28 23:37:32 -0300

    11:37 28/12/2023

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,125 +1,135 @@
 // ---------  HOOKS  ---------
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 // ---------  STYLES  ---------
-import './App.css';
+import "./App.css";
 // ---------  COMPONENTS  ---------
-import Nav from './components/Nav/Nav.jsx';
-import About from './views/About/About.jsx';
-import Home from './views/Home/Home.jsx'
-import Landing from './views/Landing/Landing.jsx'
+import Nav from "./components/Nav/Nav.jsx";
+import About from "./views/About/About.jsx";
+import Home from "./views/Home/Home.jsx";
+import Landing from "./views/Landing/Landing.jsx";
 import Favorites from "./views/Favorites/Favorites";
-import Deatil from './views/Deatil/Deatil.jsx';
+import Deatil from "./views/Deatil/Deatil.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import UpScroll from "./components/UpScroll/UpScroll";
 // ---------  REACT DOM  ---------
-import {Routes, Route, useNavigate} from 'react-router-dom'
+import { Routes, Route, useNavigate } from "react-router-dom";
 // ---------  ACTIONS  ---------
 import { deleteChars } from "./redux/actions.js";
 import axios from "axios";
-axios.defaults.baseURL = "https://rickand-morty-production.up.railway.app/rickandmorty/"
-// axios.defaults.baseURL = "http://localhost:3001/rickandmorty/"
+// axios.defaults.baseURL = "https://rickand-morty-production.up.railway.app/rickandmorty/"
+axios.defaults.baseURL = "http://localhost:3001/rickandmorty/";
 
 function App() {
-   const [characters, setCharacters] = useState([])
-   const dispatch = useDispatch();
-   
-   const navigate = useNavigate(); //ruta actual
-   const [access, setAccess] = useState(false); // estado de acceso
-   // const EMAIL = 'lautibunko@gmail.com'; //mock
-   // const PASSWORD = '43244847'; //mock
+  const [characters, setCharacters] = useState([]);
+  const dispatch = useDispatch();
 
-   // const login = (userData) => {
-   //    if(userData.email === EMAIL && userData.password === PASSWORD){
-   //       setAccess(true);
-   //       navigate('/home');
-   //    } else {
-   //       window.alert('No ha ingresado los datos correctos')
-   //    }
-   // }
+  const navigate = useNavigate(); //ruta actual
+  const [access, setAccess] = useState(false); // estado de acceso
+  // const EMAIL = 'lautibunko@gmail.com'; //mock
+  // const PASSWORD = '43244847'; //mock
 
+  // const login = (userData) => {
+  //    if(userData.email === EMAIL && userData.password === PASSWORD){
+  //       setAccess(true);
+  //       navigate('/home');
+  //    } else {
+  //       window.alert('No ha ingresado los datos correctos')
+  //    }
+  // }
 
-   const login = async (userData) => {
-      try {
-         // const { email, password } = userData;
-         const email = 'correo@mail.com';
-         const password = 'password';
-         const URL = '/login';
-         const { data }  = await axios.get(URL +`?email=${email}&password=${password}`)
-         setAccess(data.access);
-         data.access && navigate('/home');
-      } catch (error) {
-         if(!error.response) return window.alert(error.message)
-         window.alert(error.response.statusText)
+  const login = async (userData) => {
+    try {
+      // const { email, password } = userData;
+      const email = "correo@mail.com";
+      const password = "password";
+      const URL = "/login";
+      const { data } = await axios.get(
+        URL + `?email=${email}&password=${password}`
+      );
+      setAccess(data.access);
+      data.access && navigate("/home");
+    } catch (error) {
+      if (!error.response) return window.alert(error.message);
+      window.alert(error.response.statusText);
+    }
+  };
+  const logOut = () => {
+    setAccess(false);
+    setCharacters([]);
+    dispatch(deleteChars());
+  };
+  const buscarPersonaje = (characters, newCharacter) => {
+    return characters.some((character) => character.id === newCharacter.id);
+  };
+  const searchCharacters = async (id) => {
+    try {
+      const { data } = await axios(`/character/${id}`);
+
+      if (buscarPersonaje(characters, data)) {
+        window.alert("Ya hay personajes con ese ID");
+      } else {
+        setCharacters((oldChars) => [...oldChars, data]);
+        // setCharacters([...characters, data])
       }
-   }
-   const logOut = () => {
-      setAccess(false);
-      setCharacters([])
-      dispatch(deleteChars())
-   }
-   const buscarPersonaje = (characters, newCharacter) => {
-      return characters.some(character => character.id === newCharacter.id);
-   }
-   const searchCharacters = async (id) => {
-      try {
-         const { data } = await axios(`/character/${id}`)
+    } catch (error) {
+      window.alert(error.message);
+    }
+  };
+  const onClose = (id) => {
+    let newCharacters = characters.filter(
+      (character) => character.id !== Number(id)
+    );
+    setCharacters(newCharacters);
+  };
 
-         if(buscarPersonaje(characters, data)){
-            window.alert('Ya hay personajes con ese ID');
-         } else {
-            setCharacters((oldChars) =>[...oldChars, data])
-            // setCharacters([...characters, data])
-         }
-      } catch (error) {
-         window.alert(error.message)
-      }
-      
-   }
-   const onClose = (id) => {
-      let newCharacters = characters.filter(character => character.id !== Number(id))
-      setCharacters(newCharacters);
-   }
+  // const searchCharacters = (id) => {
+  //    fetch(`http://localhost:3001/rickandmorty/character/${id}`)
+  //    // fetch(`https://rickandmortyapi.com/api/character/${id}`)
+  //    .then((res) => res.json())
+  //    .then((data) => {
+  //       console.log(data)
+  //       if(buscarPersonaje(characters, data)){
+  //          window.alert('Ya hay personajes con ese ID');
+  //       }
+  //       else if(data.name){
+  //          setCharacters((oldChars) =>[...oldChars, data])
+  //          // setCharacters([...characters, data])
+  //       } else {
+  //          window.alert('No hay personajes con ese ID');
+  //       }
+  //    })
+  // }
 
-   // const searchCharacters = (id) => {
-   //    fetch(`http://localhost:3001/rickandmorty/character/${id}`)
-   //    // fetch(`https://rickandmortyapi.com/api/character/${id}`)
-   //    .then((res) => res.json())
-   //    .then((data) => {
-   //       console.log(data)
-   //       if(buscarPersonaje(characters, data)){
-   //          window.alert('Ya hay personajes con ese ID');
-   //       }
-   //       else if(data.name){
-   //          setCharacters((oldChars) =>[...oldChars, data])
-   //          // setCharacters([...characters, data])
-   //       } else {
-   //          window.alert('No hay personajes con ese ID');
-   //       }
-   //    })
-   // }
-   
-   // USE EFFECT ------------
-   useEffect(() => {
-      !access && navigate('/');
-   }, [access]);
+  // USE EFFECT ------------
+  useEffect(() => {
+    !access && navigate("/");
+  }, [access]);
 
-   return (
-      <div className='App'>
-         <div className="background-container"/>
-         <Nav onSearch={searchCharacters} logOut={logOut}/>
-         <Routes>
-            <Route path='/'element={<Landing login={login}/>}/>
-            <Route path='/home' element={<Home characters={characters} onClose={onClose} setCharacters={setCharacters}/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/deatil/:id' element={<Deatil/>}/>
-            <Route path='/favorites' element={<Favorites onClose={onClose}/>}/>
-         </Routes>
-         <UpScroll/>
-         <Footer/>
-      </div>
-   );
-
+  return (
+    <div className="App">
+      <div className="background-container" />
+      <Nav onSearch={searchCharacters} logOut={logOut} />
+      <Routes>
+        <Route path="/" element={<Landing login={login} />} />
+        <Route
+          path="/home"
+          element={
+            <Home
+              characters={characters}
+              onClose={onClose}
+              setCharacters={setCharacters}
+            />
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/deatil/:id" element={<Deatil />} />
+        <Route path="/favorites" element={<Favorites onClose={onClose} />} />
+      </Routes>
+      <UpScroll />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,23 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import store from "../src/redux/store";
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'))
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
-)
-
-// root.render(
-//   <BrowserRouter>
-//     <App/>
-//   </BrowserRouter>
-// )
+  document.getElementById("root")
+);
