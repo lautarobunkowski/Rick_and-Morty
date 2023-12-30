bdfdff784685b003505d07b410eaeb02392eb4a7
lautaro_Bunkowski <lautibunko@gmail.com>
2023-12-30 09:30:49 -0300

    prueba estoy cansadoooo

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,11 @@
 const server = require("./src/app.js");
-const { conn } = require("./src//DB_connection.js");
+const { conn } = require("./src/DB_connection.js");
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-// conn.sync({force: true})
-// .then(() => {
-//     server.listen(PORT, () => {
-//         console.log("Server raised in port: " + PORT)
-//     })
-// })
-
-server.listen(PORT, () => {
-    console.log("Server raised in port: " + PORT)
+conn.sync({force: false})
+.then(() => {
+    server.listen(PORT, () => {
+        console.log("Server raised in port: " + PORT)
+    })
 })
