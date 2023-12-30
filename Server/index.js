bb8a5f152eabcb5c2984189175689575cf502dee
lautaro_Bunkowski <lautibunko@gmail.com>
2023-12-30 08:56:05 -0300

    prueba sin BD

--- Server/index.js
+++ Server/index.js
@@ -3,9 +3,13 @@ const { conn } = require("./src//DB_connection.js");
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
-conn.sync({force: true})
-.then(() => {
-    server.listen(PORT, () => {
-        console.log("Server raised in port: " + PORT)
-    })
+// conn.sync({force: true})
+// .then(() => {
+//     server.listen(PORT, () => {
+//         console.log("Server raised in port: " + PORT)
+//     })
+// })
+
+server.listen(PORT, () => {
+    console.log("Server raised in port: " + PORT)
 })
